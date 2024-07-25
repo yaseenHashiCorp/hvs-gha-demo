53faf5018192d1a2e87d43fdbe877061b01a7b07
kmccarthyhashi <107080002+kmccarthyhashi@users.noreply.github.com>
2024-07-25 14:56:22 -0500

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -4,14 +4,14 @@
 
 cat << EOM > /var/www/html/index.html
 <html>
-  <head><title>Meow!</title></head>
+  <head><title>Hi!</title></head>
   <body>
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
-  <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  <center><h2>Hello World!</h2></center>
+  Welcome to Team 2's app. The secret is: .
   <!-- END -->
 
   </div>
