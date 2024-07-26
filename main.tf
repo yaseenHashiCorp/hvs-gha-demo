1a51d9f4a53d9f8b618f84d73b947362328a76a9
kmccarthyhashi <107080002+kmccarthyhashi@users.noreply.github.com>
2024-07-26 10:56:36 -0500

    Update main.tf
    add space

--- main.tf
+++ main.tf
@@ -195,7 +195,7 @@ resource "null_resource" "configure-cat-app" {
       "sudo systemctl start apache2",
       "sudo chown -R ubuntu:ubuntu /var/www/html",
       "chmod +x *.sh",
-      "PLACEHOLDER=${var.placeholder} WIDTH=${var.width} HEIGHT=${var.height} PREFIX=${var.prefix} APP_SECRET=${data.hcp_vault_secrets_secret.web_application.secret_name}./deploy_app.sh",
+      "PLACEHOLDER=${var.placeholder} WIDTH=${var.width} HEIGHT=${var.height} PREFIX=${var.prefix} APP_SECRET=${data.hcp_vault_secrets_secret.web_application.secret_name} ./deploy_app.sh",
       "sudo apt -y install cowsay",
       "cowsay Mooooooooooo!",
     ]
