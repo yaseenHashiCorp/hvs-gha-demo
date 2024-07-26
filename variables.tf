253c5abd69448fd9f5654ac2df7fb67c5f7116c8
kmccarthyhashi <107080002+kmccarthyhashi@users.noreply.github.com>
2024-07-26 10:32:53 -0500

    Update variables.tf
    add hvs_secret to var.tf

--- variables.tf
+++ variables.tf
@@ -52,6 +52,11 @@ variable "width" {
   description = "Image width in pixels."
 }
 
+variable "hvs_secret" {
+  value = data.hcp_vault_secrets_app.web_application.secrets
+  sensitive = true
+}
+
 variable "placeholder" {
   default     = "placekitten.com"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
