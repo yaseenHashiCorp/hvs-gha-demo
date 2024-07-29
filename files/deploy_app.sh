f0b41e268c31bfb3558a5a64970adb971d3f70fd
kmccarthyhashi <107080002+kmccarthyhashi@users.noreply.github.com>
2024-07-29 15:40:52 -0500

    Update deploy_app.sh
    making the html a little more fun

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -4,14 +4,14 @@
 
 cat << EOM > /var/www/html/index.html
 <html>
-  <head><title>Hi!</title></head>
+  <head><title>Team 2 Rocks!</title></head>
   <body>
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
-  <center><h2>Hello World!</h2></center>
-  Welcome to Team 2's app. The secret is: ${APP_SECRET} .
+  <center><h2>Hello Corporate SE Team!</h2></center>
+  Welcome to Team 2's app. The name of our HVS secret is: ${APP_SECRET} .
   <!-- END -->
 
   </div>
